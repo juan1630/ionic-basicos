7e657d5e18a84a7db23db2c5a0e0e18eb714f7eb
José Juan Patrón Guerrero <josejuanpatron1630@gmail.com>
2021-01-15 18:34:29 -0600

    Inputs

---
+++ src/app/pages/input/input.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.page.html',
+  styleUrls: ['./input.page.scss'],
+})
+export class InputPage implements OnInit {
+
+
+  public nombre: String = "";
+ 
+  public usuario = {
+    nombre:'',
+    email:'',
+    password:"",
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(  formulario: NgForm ){
+    console.log(this.usuario);
+    console.log( formulario );
+  }
+
+}
