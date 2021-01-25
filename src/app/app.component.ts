386a065fe189ac747883b3affd5536d8c6f0579e
José Juan Patrón Guerrero <josejuanpatron1630@gmail.com>
2021-01-25 00:18:52 -0600

    Se vio el ion menu y ion menu button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,28 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { DataService } from './services/data.service';
+import { Componente } from './interfaces/interfaces';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
+
 export class AppComponent {
+
+  public componentes:Observable<Componente[]>;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private dataService: DataService
   ) {
     this.initializeApp();
+    this.componentes = this.dataService.getMenuOpts();
   }
 
   initializeApp() {
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { Componente } from 'src/app/interfaces/interfaces';
+import { DataService } from 'src/app/services/data.service';
 
-interface Componente {
-  icon: string;
-  name: string;
-  redirectTo: string;
 
-} 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -13,84 +12,20 @@ interface Componente {
 })
 export class HomePage implements OnInit {
 
-  public components: Componente[] =
-  [
-    {
-    icon: 'american-football-outline',
-    name: 'action-sheet',
-    redirectTo: '/action-sheet'
-  },
-  {
-    icon: 'logo-apple-appstore',
-    name: 'Alert',
-    redirectTo: '/alert'
-  },
-  {
-    icon: 'beaker-outline',
-    name: 'avatar',
-    redirectTo: '/avatar'
-  },
-  {
-    icon: 'radio-button-off-outline',
-    name:'Buttons',
-    redirectTo: '/button'
-  },
-  {
-    icon: 'card-outline',
-    name: 'Cards',
-    redirectTo: '/card'
-  },
-  {
-    icon: 'checkmark-circle-outline',
-    name: 'Checks',
-    redirectTo: '/check'
-  },
-  {
-    icon: 'calendar-outline',
-    name: 'DateTime',
-    redirectTo: '/date-time'
-  },
-  {
-    icon: 'car-outline',
-    name: 'FAB',
-    redirectTo: '/fab'
-  },
-  {
-    icon: 'grid-outline',
-    name:'Grid',
-    redirectTo: '/grid'
-  }
-  ,
-  {
-    icon: 'infinite-outline',
-    name:'Infinite Scroll',
-    redirectTo: '/infinite-scroll'
-  },
-  {
-    icon: 'hammer-outline',
-    name:'Input Forms',
-    redirectTo: '/input'
-  },
-  {
-    icon: 'list-outline',
-    name:'List Sliding',
-    redirectTo: '/list'
-  },
-  {
-    icon: 'list-three-outline',
-    name:'List Reorder',
-    redirectTo: '/list-reorder'
-  },
-  {
-    icon: 'refresh-circle-outline',
-    name:'Loading',
-    redirectTo: '/loading'
-  },
-];
+  componentes:Observable<Componente[]>;
 
-  constructor() { }
+  constructor(
+    private menuCtrl: MenuController,
+    private dataservice: DataService
+  ) { }
 
   ngOnInit() {
+      this.componentes = this.dataservice.getMenuOpts();
+  }
+
+  mostrarMenu(){
+    //mnaodmamos el id del menu que queremos abrir
+    this.menuCtrl.open( 'first' );
   }
 
 }
